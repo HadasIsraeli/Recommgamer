2064ca303c3ef8e69ad44ec3396e8078da09045e
HADASISRAELI\hadas <hadas25496@gmail.com>
2022-07-17 19:00:09 +0300

    search game

--- src/components/Register.js
+++ src/components/Register.js
@@ -16,34 +16,43 @@ function Register() {
     let history = useHistory();
     const { user, setUser } = useContext(SearchContext);
 
-    const state = {
-        users: [
-            {
-                name: 'Hadas', userName: 'Dusa', age: 26, gender: 'blue', id: 1, password: null, mail: null, LoggedIn: false
-            },
-            {
-                name: 'Inbar', userName: 'Bari', age: 27, gender: 'pink', id: 2, password: null, mail: null, LoggedIn: false
-            },
-            {
-                name: 'Grisha', userName: 'Greg', age: 29, gender: 'RED', id: 3, password: null, mail: null, LoggedIn: false
-            }
-        ]
-    };
+    const [error, SetError] = useState("");
+    // SetError('Details do not match! Please Register :) ');
 
-    const [users_list, setState] = useState({ users: state.users });
+
+
+    // const state = {
+    //     users: [
+    //         {
+    //             name: 'Hadas', userName: 'Dusa', age: 26, gender: 'blue', id: 1, password: null, mail: null, LoggedIn: false
+    //         },
+    //         {
+    //             name: 'Inbar', userName: 'Bari', age: 27, gender: 'pink', id: 2, password: null, mail: null, LoggedIn: false
+    //         },
+    //         {
+    //             name: 'Grisha', userName: 'Greg', age: 29, gender: 'RED', id: 3, password: null, mail: null, LoggedIn: false
+    //         }
+    //     ]
+    // };
+
+    // const [users_list, setState] = useState({ users: state.users });
+
     const usersCollectionRef = collection(db, "users");
 
     const addUser = (new_user) => {
+        
         console.log(new_user);
         new_user.id = Math.random();
         new_user.type = 'basic';
         new_user.LoggedIn = true;
-        let users = [...state.users, new_user];
-        setState({
-            users: users
-        });
+
+        // let users = [...state.users, new_user];
+        // setState({
+        //     users: users
+        // });
+
         setUser(new_user);
-        console.log('users list: ', new_user, users_list);
+        // console.log('users list: ', new_user, users_list);
 
         addDoc(usersCollectionRef, {
             name: new_user.name,
@@ -56,12 +65,20 @@ function Register() {
             mail: new_user.mail,
         });
 
+
+
+
+
+
+
         history.push('/WelcomePage');
 
     }
 
 
-    console.log('The User Is: ', user, users_list);
+    // console.log('The User Is: ', user, users_list);
+    console.log('The User Is: ', user);
+
 
     return (
         <div className="App">
