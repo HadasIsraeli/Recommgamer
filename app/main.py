329f7040f6d026597fd77c4565bae43cefebbdb0
Sapozh14 <alonsapozh14@gmail.com>
2022-08-14 20:41:05 +0300

    please work

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def postME():
    print("data recived: " + str(request_data))
 
    game_name = request_data["user input"]["game_name"].lower()
-   data = pd.read_csv("app\df\clean_df_only_reviews.csv", encoding='utf-8')
+   data = pd.read_csv("app/df/clean_df_only_reviews.csv", encoding='utf-8')
    gameNames = data['Game Title'].to_list()
    gameNamesLowerCase = [x.lower() for x in gameNames]
    res = any(game_name in string for string in gameNamesLowerCase)
