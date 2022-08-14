2fcea0455483ab0db9abc981d4cd0565319d8127
Sapozh14 <alonsapozh14@gmail.com>
2022-08-14 20:29:12 +0300

    Changed CSV PATH

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def postME():
    print("data recived: " + str(request_data))
 
    game_name = request_data["user input"]["game_name"].lower()
-   data = pd.read_csv("app/df/clean_df_only_reviews.csv", encoding='utf-8')
+   data = pd.read_csv("app\df\clean_df_only_reviews.csv", encoding='utf-8')
    gameNames = data['Game Title'].to_list()
    gameNamesLowerCase = [x.lower() for x in gameNames]
    res = any(game_name in string for string in gameNamesLowerCase)
@@ -29,7 +29,6 @@ def postME():
       res_data = -1
 
 
-   res_data = handle_re.response(request_data) #method that returns the response json
 
    response = app.response_class(
       response=json.dumps(res_data),
