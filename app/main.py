88f0da2fc4c62153a2c21926e10efa46969311ca
Sapozh14 <alonsapozh14@gmail.com>
2022-08-14 20:06:56 +0300

    Readded search for game name in DS

--- app/main.py
+++ app/main.py
@@ -19,14 +19,14 @@ def postME():
    print("data recived: " + str(request_data))
 
    game_name = request_data["user input"]["game_name"].lower()
-   data = pd.read_csv("app/df/metacritic_critic_reviews.csv", encoding='utf-8')
-   # gameNames = data['name'].to_list()
-   # gameNamesLowerCase = [x.lower() for x in gameNames]
-   # res = any(game_name in string for string in gameNamesLowerCase)
-   # if(res):
-   #    res_data = handle_re.response(request_data) #method that returns the response json
-   # else:
-   #    res_data = -1
+   data = pd.read_csv("app/df/clean_df_only_reviews.csv", encoding='utf-8')
+   gameNames = data['Game Title'].to_list()
+   gameNamesLowerCase = [x.lower() for x in gameNames]
+   res = any(game_name in string for string in gameNamesLowerCase)
+   if(res):
+      res_data = handle_re.response(request_data) #method that returns the response json
+   else:
+      res_data = -1
 
 
    res_data = handle_re.response(request_data) #method that returns the response json
